d1dbbda307eb9ebfcdf976674d6e13597223f7ac
Guilherme Lahr <guilhermelahr@gmail.com>
2021-11-22 14:29:29 +0000

    new
    Change-Id: I52988ce744bac9e2a64c704450ed0b1b2f5babff

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("test.test_git_parallel.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"test/test_git_parallel/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
